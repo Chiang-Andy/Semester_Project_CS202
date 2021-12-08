75b02f051fe95b8ed5bbb7910cbc9fd23ef74308
Andy Chiang <andychiang@Andys-MacBook-Air.local>
2021-12-08 01:23:22 -0800

    SemesterSolution

--- wav.cpp
+++ wav.cpp
@@ -11,13 +11,6 @@ Wav::~Wav() {
 // 1-8 bits: unsigned int
 // 9+ bits: signed int
 
-/**
- * @brief 
- * 
- * @param filename 
- * @return true 
- * @return false 
- */
 bool Wav::readFile (const std::string &filename){
 
 	std::ifstream file(filename, std::ios::binary | std::ios::in);
@@ -31,13 +24,7 @@ bool Wav::readFile (const std::string &filename){
 			/*for (int i = 0; i < 256; i++) {
 				std::cout << (int)buffer8[i] << " ";
 			}*/
-		if(std::string {wavHeader.riff_header, 4} != "RIFF"){
-			return false;
-		}
-		if(std::string {wavHeader.wave_header, 4} != "WAVE"){
-			return false;
-		} 
-		else {
+		} else {
 			buffer16 = new short[wavHeader.data_bytes];
 			file.read((char*)buffer16,wavHeader.data_bytes);
 			/*for (int i = 0; i < 256; i++) {
@@ -51,10 +38,6 @@ bool Wav::readFile (const std::string &filename){
 	}
 }
 
-/**
- * @brief 
- * 
- */
 void Wav::printMetadata() {
 	std::cout << "Filename: " << name << std::endl;
 	std::cout << "Sample rate: " << wavHeader.sample_rate << std::endl;
@@ -69,25 +52,16 @@ void Wav::printMetadata() {
 		default:
 			std::cout << "Number of channels: " << wavHeader.num_channels << std::endl;
 	}
-	//std::cout << "data bytes: " << wavHeader.data_bytes << std::endl;
+	std::cout << "data bytes: " << wavHeader.data_bytes << std::endl;
 	
 }
 
-/**
- * @brief 
- * 
- * @param outFileName 
- */
 void Wav::copyFile(const std::string &outFileName) {
 	std::ifstream inf(name, std::ios::binary | std::ios::out);
 	std::ofstream outf(outFileName, std::ios::binary | std::ios::out);
 	outf << inf.rdbuf();
 }
 
-/**
- * @brief 
- * 
- */
 void Wav::writeFile() {
 	std::ofstream file(name, std::ios::binary | std::ios::out);
 	if(file.is_open())
@@ -108,46 +82,22 @@ void Wav::writeFile() {
 		}
 }
 
-/**
- * @brief 
- * 
- * @return unsigned* 
- */
 unsigned char* Wav::getBuffer8() {
 	return buffer8;
 }
 
-/**
- * @brief 
- * 
- * @return short* 
- */
 short* Wav::getBuffer16() {
 	return buffer16;
 }
 
-/**
- * @brief 
- * 
- * @return int 
- */
 int Wav::getBuffer8Size() const {
 	return wavHeader.data_bytes;
 }
 
-/**
- * @brief 
- * 
- * @return int 
- */
 int Wav::getBuffer16Size() const {
 	return wavHeader.data_bytes;
 }
 
-/**
- * @brief 
- * 
- */
 void Wav::normal() {
 	Processor * p = new Normal();
 	if (wavHeader.num_channels == 1) {
@@ -156,13 +106,6 @@ void Wav::normal() {
 		p->processBuffer16(getBuffer16(), getBuffer16Size());
 	}
 }
-
-/**
- * @brief 
- * 
- * @param factor 
- * @param seconds 
- */
 void Wav::echo(float factor, float seconds) {
 	Processor * p = new Echo(factor, seconds, wavHeader.sample_rate, wavHeader.num_channels);
 	if (wavHeader.num_channels == 1) {
@@ -171,12 +114,6 @@ void Wav::echo(float factor, float seconds) {
 		p->processBuffer16(getBuffer16(), getBuffer16Size());
 	}
 }
-
-/**
- * @brief 
- * 
- * @param factor 
- */
 void Wav::gain(float factor) {
 	Processor * p = new Gain(factor);
 	if (wavHeader.num_channels == 1) {
