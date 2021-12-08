fa1e82ef43c051c3c01f6d330e8c46434798f973
Andy Chiang <andychiang@Andys-MacBook-Air.local>
2021-12-07 23:50:30 -0800

    solution

--- Seanns-Semester-Project/processor.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class Processor {
-public:
-	virtual void processBuffer8(unsigned char* buffer8, int bufferSize) = 0;
-	virtual void processBuffer16(short* buffer16, int bufferSize) = 0;
-};
